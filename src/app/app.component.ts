1dcd194bb3a94ed3d602bf373cd8935bf6ec1707
Luis Polio <guspolio@outlook.es>
2024-07-31 16:59:53 -0600

    Se comienza tabla de inversión

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { UserInputComponent } from "./components/user-input/user-input.component";
 import { InvestmentModel } from './investment.model';
+import { InvestmentResultsComponent } from "./components/investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent]
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
 export class AppComponent {
   
